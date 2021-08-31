d8e9374bc163025dd9cd438ccabd6dcb49331fc5
Denzel <dbledsoe374@gmail.com>
2021-08-31 18:25:13 -0400

    Feature Data Done

---
+++ CharacterApp.Data/Ability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterApp.Data
+{
+    class Ability
+    {
+        public string SuperSpeed { get; set; }
+        public string Phasing { get; set; }
+        public string Magic { get; set; }
+        public string Flight { get; set; }
+        public string Telepathy { get; set; }
+        public string Healing { get; set; }
+        public string Invisibility { get; set; }
+    }
+}
---
+++ CharacterApp.Data/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterApp.Data
+{
+    class Feature
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public List<Ability> Abilities { get; set; }
+
+        [ForeignKey(nameof(Characters))]
+        public Character Characters { get; set; }
+    }
+}
