fd0af7e70e9ae2541abb731bd5a48ad8442e1a35
Nicholas Stoner <nicholas.c.stoner@gmail.com>
2021-08-31 20:07:40 -0400

    nicks changes

---
+++ CharacterApp.Models/CharacterModels/CharacterDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterApp.Models.CharacterModels
+{
+    public class CharacterDetail
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+        public List<Feature> MyProperty { get; set; }
+    }
+}
