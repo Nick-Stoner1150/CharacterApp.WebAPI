c2700dd544d99313377c2d5672d486552045f7e6
Denzel <dbledsoe374@gmail.com>
2021-08-31 20:25:21 -0400

    Added Feature PUT method in FeatureServices.

--- CharacterApp.Data/Feature.cs
+++ CharacterApp.Data/Feature.cs
@@ -12,12 +12,21 @@ namespace CharacterApp.Data
     {
         [Key]
         public int Id { get; set; }
+
         public string SuperSpeed { get; set; }
+
         public string Phasing { get; set; }
+
         public string Magic { get; set; }
+
         public string Flight { get; set; }
+
         public string Telepathy { get; set; }
+
         public string Healing { get; set; }
+
         public string Invisibility { get; set; }
+
+        public virtual List<Character> Characters { get; set; }
     }
 }
--- CharacterApp.Services/FeatureService/FeatureService.cs
+++ CharacterApp.Services/FeatureService/FeatureService.cs
@@ -1,4 +1,5 @@
 ﻿using CharacterApp.Data;
+using CharacterApp.Models.CharacterModels;
 using CharacterApp.Services.FeatureModel;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,27 @@ namespace CharacterApp.Services.FeatureService
             using (var ctx = new ApplicationDbContext())
             {
                 ctx.Features.Add(entity);
+                return await ctx.SaveChangesAsync() > 0;
+            }
+        }
+
+        public async Task<bool> Put(FeatureEdit featureEdit, int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var oldData = await ctx.Features.FindAsync(id);
+                if (oldData is null)
+                {
+                    return false;
+                }
+                oldData.SuperSpeed = featureEdit.SuperSpeed;
+                oldData.Phasing = featureEdit.Phasing;
+                oldData.Magic = featureEdit.Magic;
+                oldData.Flight = featureEdit.Flight;
+                oldData.Telepathy = featureEdit.Telepathy;
+                oldData.Healing = featureEdit.Healing;
+                oldData.Invisibility = featureEdit.Invisibility;
+
                 return await ctx.SaveChangesAsync() > 0;
             }
         }
